fadc58b6d9633a7de7138672d18c014a5dd835d0
Laboratorio2 <cristhianchavarrianic@gmail.com>
2018-02-11 15:53:00 -0600

    Añadiendo index

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Laboratorio2
- * Date: 11/02/2018
- * Time: 14:58
- */
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi pagina</title>
+</head>
+<body>
+<header style="background-color: black; width: 100%; height: 200px;">
+    <section style="background-color: blue; width: 100%; height: 150px;"></section>
+    <nav style="background-color: green; width: 100%; height: 100px;"></nav>
+</header>
 
-echo"hi";
+</body>
+</html>
